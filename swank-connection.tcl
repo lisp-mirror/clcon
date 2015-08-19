c5bd070bff1d332e9c9121bed1c4dad48371a11b
Unknown <denis@i5>
2015-08-19 17:25:36 +0300

    go on

--- swank-connection.tcl
+++ swank-connection.tcl
@@ -1,13 +1,16 @@
 ## Swank connection object
 
 namespace eval ::swcnn {
-    ## two level array of connections
-    #variable Connections
+    variable SwankConnectionCounter 0
+
+    # name of connection
+    variable CurrentSwankConnection {}
 }
 
 
 proc ::swcnn::GenSwankConnectionCounter {} {
- variable SwankConnectionCounter
+    variable SwankConnectionCounter
+    
  if {![info exists SwankConnectionCounter]} {
     set SwankConnectionCounter 1
  } else {
@@ -16,27 +19,69 @@ proc ::swcnn::GenSwankConnectionCounter {} {
  return $SwankConnectionCounter
 }
 
+# Throws an error if can't close sock
+# Deletes the connection
+proc ::swcnn::TerminateConnection {name} {
+    variable CurrentSwankConnection
+    upvar \#0 $name con
+
+    if { $CurrentSwankConnection eq $name } {
+        set CurrentSwankConnection {}
+    }
+    if {[lsearch {socket_only initialized} $con(state)]} {
+        set sock $con(sock)
+        unset $name
+        close $sock
+    } else {
+        unset $name
+    }
+}
+
+proc ::swcnn::TerminateCurrentConnection {} {
+    variable CurrentSwankConnection
+    if { $CurrentSwankConnection ne {} } {
+        TerminateConnection $CurrentSwankConnection
+    }
+}
+
+
+# connection: an array variable named SwankConnectionNNN
+# array members:
+# sock - channel
+# state 
+#   new
+#   socket_only
+#   initialized
+#   dead (socket eof)
+# SWANKIsInSyncMode? we only support one SWANK now, so don't matter
+
 # instead of ::tkcon::NewSwank
 # Results:	It will create a socket, and attach console to it
 # FIXME console vs interpreter - can we have one interpreter (or one swank)
 # for several tabs? This is noncence, but what does tkcon think?
 proc ::swcnn::MakeSwankConnection {host port} {
+    variable CurrentSwankConnection
+
+    TerminateCurrentConnection
+
     set id [GenSwankConnectionCounter]
     set name "::swcnn::SwankConnection$id"
     variable $name
     upvar \#0 $name con
-    array set $name {}
+    array set con {state new}
     #set con(sock) 123123213213
     #puts $con(sock)
     if {[catch {
         set sock [socket $host $port]
 	set con(sock) $sock
         fconfigure $sock -encoding utf-8
+        set con(state) socket_only
     } err]} {
 	tk_messageBox -title "Socket Connection Error" \
 		-message "Unable to connect to \"$host:$port\":\n$err" \
             -icon error -type ok
     } else {
+        set CurrentSwankConnection $name
 	::tkcon::AttachSwank $name
     }
 }
