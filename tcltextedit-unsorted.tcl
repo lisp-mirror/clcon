b1e7627af1e7d1a39707d8c4cd01186c25d2baf3
Unknown <denis@i5>
2015-09-07 10:55:05 +0300

    fixed tcltextedit logger (proc c )

--- tcltextedit-unsorted.tcl
+++ tcltextedit-unsorted.tcl
@@ -1,12 +1,14 @@
 # Unsorted code borrowed from tcltextedit.
 
-if { $::tkcon::OPT(putd-enabled) == 1 } {
-    catch {destroy .logger}
-    toplevel .logger
-    wm title .logger "Logg"
-    text .logger.text 
-    pack .logger.text
-    wm protocol  .logger WM_DELETE_WINDOW { destroy . }
+proc MaybeCreateLogger {} {
+    if { $::tkcon::OPT(putd-enabled) == 1 } {
+        catch {destroy .logger}
+        toplevel .logger
+        wm title .logger "Logg"
+        text .logger.text 
+        pack .logger.text
+        wm protocol  .logger WM_DELETE_WINDOW { destroy . }
+    }
 }
 
 proc c {args} {
@@ -20,7 +22,8 @@ proc c {args} {
 	} else { set result "Root --> $args"  }
 
 	# if {$debug_messages==2} Make putd-enabled allow for 2? 
-        if { 1 == 1 } { 
+        if { 1 == 1 } {
+            MaybeCreateLogger
             .logger.text insert end $result
             .logger.text see end
 	} else { puts stdout $result}
