041a31e9799dd5b4af2cf170be2e10827fa1bf0b
Unknown <denis@i5>
2015-09-12 20:24:12 +0300

    oduvanchik backend stub

--- text2odu.lisp
+++ text2odu.lisp
@@ -1,8 +1,13 @@
 ; -*- coding : utf-8 ; Encoding : utf-8 ; system :clcon-server ; -*-
 ; see also text2odu.tcl
 
-(in-package :clcon-server)
+(in-package :clco)
 
-(defun send-event-to-oduvanchik (text)
-  "
+(defun make-oduvan-backend-buffer (tcl-id)
+  "Called from tcl when text is created"
+  (print `(make-oduvan-backend-buffer ,tcl-id))
+  )
 
+(defun notify-oduvan-on-tcl-text-change (&rest args)
+  "Called from RoInsert, RoDelete, RoReplace"
+  (print `(notify-oduvan-on-tcl-text-change ,@args)))
