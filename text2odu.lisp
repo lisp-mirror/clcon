794251b06521e65002600f2c5b07e34bae7b7ce2
Unknown <denis@i5>
2015-09-14 22:47:56 +0300

    Posting typed and parsed events from tcl onto *text2odu-event-queue*

--- text2odu.lisp
+++ text2odu.lisp
@@ -15,56 +15,94 @@
 
 
 (deftype text2odu-event-kind ()
-  '(member construct-backend-buffer tcl-text-insert tcl-text-delete destroy-backend-buffer))
+  '(member construct-backend-buffer before-tcl-text-insert before-tcl-text-delete destroy-backend-buffer))
+
+(defstruct row-col
+  (row 0 :type integer)
+  (col 0 :type integer)
+  )
 
 (defstruct text2odu-event
   (kind (budden-tools:mandatory-slot 'kind) :type text2odu-event-kind)
-  ...
+  (clcon_text-pathname (budden-tools:mandatory-slot 'clcon_text-pathname) :type string)
+  (string nil :type (or null string)) ; string to insert
+  (beg nil :type (or null row-col)) ; begin index
+  (end nil :type (or null row-col))   ; end index
   )
         
-  
+(defmacro --> (object slot)
+  "bubububu"
+  `(slot-value ,object ',slot))
 
-(defun post-oduvan-event (clcon_text-pathname &rest args)
+
+(defun parse-row-col (tcl-index)
+  "Returns row-col structure by tcl-index of kind NN.NN"
+  (let ((parsed (split-sequence:split-sequence #\. tcl-index)))
+    (assert (eq (length parsed) 2))
+    (make-row-col :row (parse-integer (first parsed))
+                  :col (parse-integer (second parsed)))
+   
+    ))
+
+;  :clcon-server
+(defun post-oduvan-event (event)
   "Can be invoked in any thread"
-  #-clcon-oduvan (print `(post-oduvan-event ,clcon_text-pathname ,@args))
+  #-clcon-oduvan (print `(post-oduvan-event ,event))
   #+clcon-oduvan
-  (bt:with-lock-held (*oduvan-input-event-queue-lock*)
-    (print `(post-oduvan-event ,clcon_text-pathname ,@args))
+  (bt:with-lock-held (*text2odu-event-queue-lock*)
+    (print `(post-oduvan-event ,event))
     (setf *text2odu-event-queue*
-          (append *text2odu-event-queue* (list clcon_text_pathname args)))
+          (append *text2odu-event-queue*
+                  (list (budden-tools:the* text2odu-event event))))
     )
   nil
   )
-  
 
 (defun notify-oduvan-construct-backend-buffer (clcon_text-pathname)
-  "Called from tcl when text is created"
-  (post-oduvan-event 'constuct-backend-buffer clcon_text-pathname)
-  )
+  "Called from tcl when text is created, from arbitrary thread"
+  (post-oduvan-event
+   (make-text2odu-event
+    :kind 'construct-backend-buffer
+    :clcon_text-pathname clcon_text-pathname
+    )))
 
 (defun nti  (clcon_text-pathname index string)
   "notify-oduvan-tcl-text-insert . Called from RoInsert"
-  (post-oduvan-event 'tcl-text-insert clcon_text-pathname index string)
-  )
+  (post-oduvan-event
+   (make-text2odu-event
+    :kind 'before-tcl-text-insert
+    :clcon_text-pathname clcon_text-pathname
+    :beg (parse-row-col index)
+    :string string
+    )))
 
 
 (defun notify-oduvan-tcl-text-delete (clcon_text-pathname beg end)
-  "Called from RoInsert"
-  (post-oduvan-event 'tcl-text-delete clcon_text-pathname beg end)
-  )
+  "Called from RoDelete before really deleting text"
+  (post-oduvan-event
+   (make-text2odu-event
+    :kind 'before-tcl-text-delete
+    :clcon_text-pathname clcon_text-pathname
+    :beg (parse-row-col beg)
+    :end (parse-row-col end)
+    )))
 
 
 (defun notify-oduvan-destroy-backend-buffer (clcon_text-pathname)
   "Called from RoInsert"
-  (post-oduvan-event 'destroy-backend-buffer clcon_text-pathname)
-  )
+  (post-oduvan-event
+   (make-text2odu-event
+    :kind 'destroy-backend-buffer
+    :clcon_text-pathname clcon_text-pathname
+    )))
 
 
 (defun test1 ()
   (notify-oduvan-construct-backend-buffer "a")
   (sleep 0.1)
-  (nti "a" 0 "b")
+  (nti "a" "0.0" "b")
   (sleep 0.1)
+  (notify-oduvan-destroy-backend-buffer "a")
   )
 
 (test1)
--- text2odu.tcl
+++ text2odu.tcl
@@ -6,10 +6,12 @@ namespace eval ::text2odu {
     # Convert text index to oduvanchik coordinate system
     # And quotes for lisp
     proc CoerceIndex {txt index} {
+        # FIXME we might want pass 'begin' and 'end' as is and process
+        # them specially on lisp side. 
         switch -exact $index {
             {} { set i2 {} }
             begin - 
-            end { set i2 $index }
+            end { set i2 [$txt index $index] }
             insert { set i2 [$txt index $index] }
             default {
                 if {[regexp {^[0-9]+\.[0-9]+$} $index]} {
