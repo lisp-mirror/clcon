6b54f6408e04c6b5016e01f847879d896da39b62
Unknown <denis@i5>
2015-10-05 19:46:15 +0300

    added -wrap option to opening file from recent menu

--- menu.recent.tcl
+++ menu.recent.tcl
@@ -15,7 +15,7 @@ namespace eval ::recent {
             if {![::edt::IsFileBeingEdited $filename]} {
                 # Here we must check if the file is not opened already
                 set label [string cat $i $filename]
-                $s add command -label $label -command [list ::edt::edit $filename] -under 0
+                $s add command -label $label -command [list ::edt::edit -type file -wrap char -- $filename] -under 0
                 incr i
                 if {$i > 9} {
                     # can't accelerate efficiently
