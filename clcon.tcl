630d32ebc16aca4b20faec74b6069690a83bfd9b
Unknown <denis@i5>
2015-10-15 15:00:50 +0300

    0.2.4

--- clcon.tcl
+++ clcon.tcl
@@ -88,7 +88,7 @@ namespace eval ::clconcmd {
 namespace eval ::tkcon {
     # when modifying this line, make sure that the auto-upgrade check
     # for version still works.
-    variable VERSION "0.2.2"
+    variable VERSION "0.2.4"
     # The OPT variable is an array containing most of the optional
     # info to configure.  COLOR has the color data.
     variable OPT
