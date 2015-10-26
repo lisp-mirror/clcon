8c3b198ab3afdf782ccfc0f1d88436c8f4aaae3a
Unknown <denis@i5>
2015-10-26 11:12:04 +0300

    Improved .help ; renamed .ed to .edit , .history to .hist; .hist accepts optional filter string.

--- clconcmd.tcl
+++ clconcmd.tcl
@@ -4,6 +4,8 @@
 
 namespace eval clconcmd {
     proc help {} {
+    puts "Best way to find a help is to watch a source - it contains comments. To find source, type in command's name with ::clconcmd:: prefix at the console (you can use completion) and then use tcl find source.
+Currently defined commands are: "
     set result1 [info procs ::clconcmd::*]
     regsub -all "::clconcmd::" $result1 "" result2
     puts $result2
@@ -13,14 +15,27 @@ namespace eval clconcmd {
     #    history
     #}
 
+    # Load filename, name is relative to clcon source directory,
+    # Name should be in unix style (/ instead of \)
     proc tcsoh {filename} {
         tkcon main TkconSourceHere $filename
     }
 
-    proc history {} {
-        tkcon main history
+    # Show history. If searchstring is given, only shows items containing 
+    # searchstring as a case insensitive glob pattern. 
+    proc hist {{searchstring ""}} {
+        if {$searchstring ne ""} {
+            foreach c [split [tkcon main history] \n] {
+                if {[string match -nocase [string cat * $searchstring *] $c]} {
+                    puts $c
+                }
+            }
+        } else {
+            tkcon main history
+        }
     }
-
+    
+        
     # Run oduvanchik command (with 
     proc o {commandNameWoPrefix} {
         ::edt::oImplementation $commandNameWoPrefix
@@ -31,7 +46,8 @@ namespace eval clconcmd {
         tkcon main ::insp::SwankInspect "*"
     }
 
-    proc ed {filename} {
+    # 
+    proc edit {filename} {
         ::edt::edit -type file -wrap char -- $filename
     }
 
