2a51892548a1394e57f3817eed364dfc945d67d3
Unknown <denis@i5>
2015-10-30 23:06:26 +0300

    clcon-tests.lisp

---
+++ test/clcon-tests.lisp
@@ -0,0 +1,28 @@
+;; Autotests for clcon. Hope will be organized later into with some unit-test 
+;; facility. Now just compile and load the file.
+
+
+(def-merge-packages::! :clcon-tests
+                       (:use :cl)
+  (:always t)
+  )
+
+(in-package :clcon-tests)
+
+
+
+(def-trivial-test::! odu-file-path-namestring.1 
+                     (string=
+                      (DIRED::ODU-FILE-PATH-NAMESTRING "c:\\Program Files\\Steel Bank Common Lisp\\1.2.16\\sbcl.exe")
+                      "c:/Program Files/Steel Bank Common Lisp/1.2.16/"
+                      )
+  t)
+
+(def-trivial-test::! odu-file-path-namestring.2
+                     (string=
+                      (DIRED::ODU-FILE-PATH-NAMESTRING "c:/Program Files/Steel Bank Common Lisp/1.2.16/sbcl.exe")
+                      "c:/Program Files/Steel Bank Common Lisp/1.2.16/"
+                      )
+  t)
+
+
