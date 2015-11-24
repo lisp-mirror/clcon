e541979c67aded4869b4a683774d273f496459dd
Unknown <denis@i5>
2015-11-24 13:29:16 +0300

    removed trash

--- swank-inspector.lisp
+++ swank-inspector.lisp
@@ -2,17 +2,7 @@
 (in-package :clco)
 
 #| 
- (defun print-one-hyperlink-tcl-source (stream text file offset)
-  "Generates tcl code which prints out one hyperlink"
-  (let ((escaped-text (cl-tk:tcl-escape text))
-        (escaped-file (tcl-escape-filename file))
-        (offset-2 (format nil "{1.0+ ~A chars}" offset))
-        )
-    (format stream "::tkcon::WriteActiveText $w ~A output {::tkcon::EditFileAtOffset ~A ~A}; $w insert output \\\n; "
-            escaped-text
-            escaped-file
-            offset-2)))
-
+ (defun print-one-hyperlink-tcl-source 
 |#
 
 
