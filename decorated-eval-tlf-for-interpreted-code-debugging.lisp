8de210c50c6b887ce2672e58e37feec95d9dc6b9
budden73 <unknown>
2016-07-16 13:53:10 +0300

    Убрал мусор

--- decorated-eval-tlf-for-interpreted-code-debugging.lisp
+++ decorated-eval-tlf-for-interpreted-code-debugging.lisp
@@ -12,15 +12,6 @@
                        (sb-c::make-definition-source-location))))
            sb-c::*source-paths*)))
     
-
-;; sb-impl::eval-tlf is defined in c:/clcon/sbcl/1.3.4/source/src/code/eval.lisp
-(defun decorated-eval-tlf (original-fn original-exp tlf-index &optional lexenv)
-  (record-sources-to-my-locations-hash)
-  (funcall original-fn original-exp tlf-index lexenv))
-
-;(decorate-function:portably-without-package-locks
-; (decorate-function:decorate-function 'sb-impl::eval-tlf 'decorated-eval-tlf))
-
 (defun decorated-find-source-paths (original-fn form tlf-num)
   "Оригинал - в c:/clcon/sbcl/1.3.4/source/src/compiler/ir1tran.lisp"
   (multiple-value-prog1 (funcall original-fn form tlf-num)
