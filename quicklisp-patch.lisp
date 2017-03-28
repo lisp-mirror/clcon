dca9929ba4d34542f60c1a80c9114bba789f831f
budden <unknown>
2017-03-28 12:14:53 +0300

    Появлялось сообщение об ошибке кодировки при попытке загрузить несуществующую систему с Русскими буквами. Исправлено

---
+++ quicklisp-patch.lisp
@@ -0,0 +1,10 @@
+; -*- coding : utf-8 ; Encoding : utf-8 ; system :clcon-server ; -*- 
+(in-package :clco)
+
+(named-readtables::in-readtable nil)
+
+(defmethod ql-dist::find-system-in-dist :around (name dist) 
+(cond
+  ((some 'russian-budden-tools:cyrillic-char-p (string name))
+   nil)
+  (t (call-next-method))))
