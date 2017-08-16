00ec947deeee881fa46d819a2292c37650a7cd71
budden <unknown>
2017-08-16 23:17:31 +0300

    window layout progresses

--- window_layout.tcl
+++ window_layout.tcl
@@ -2,7 +2,9 @@ package require Tk
 
 ## Control over window layouts
 
-namespace eval ::window_layout {} 
+namespace eval ::window_layout {
+  variable MEASURED_SCREEN_GEOMETRY
+} 
 
 ## windows positions and sizes 
 ## WINDOW_LAYOUT is a list: { name1 spec1 name2 spec2 ... }
@@ -31,3 +33,31 @@ proc ::window_layout::SetDefaultWindowLayout {} {
                       ]
 }
 
+# MeasureScreen starts a chain of events which fills MEASURED_SCREEN_GEOMETRY
+# and_then is a lambda with no arguments smth.like
+# [list {} [subst -nocommands {puts \$::window_layout::MEASURED_SCREEN_GEOMETRY}]]
+proc ::window_layout::MeasureScreen {and_then} {
+    set w .measureScreen
+    toplevel $w
+    # Trying to position it to the main display, but I have no idea would it work or not
+    wm geometry $w 1x1+1+1
+    wm attributes $w -alpha 0
+    wm deiconify $w
+    wm state $w zoomed
+    update idletasks
+    after idle [list ::window_layout::MeasureScreenPart2 $and_then]
+}
+
+
+proc ::window_layout::MeasureScreenPart2 {and_then} {
+    variable ::window_layout::MEASURED_SCREEN_GEOMETRY
+    set w .measureScreen
+    set geom [wm geometry $w]
+    regexp -- {([0-9]+)x([0-9]+)\+([0-9]+)\+([0-9]+)} $geom -> \
+      width height decorationLeft decorationTop
+    set ::window_layout::MEASURED_SCREEN_GEOMETRY [list $width $height $decorationLeft $decorationTop]
+    destroy $w
+    apply $and_then
+}
+
+#proc ::window_geometry_by_ 
