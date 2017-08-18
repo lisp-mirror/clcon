c3b1eeaaf3a72e856e2800665bc4b436737ae1d5
budden <unknown>
2017-08-18 22:46:45 +0300

    improved tests

--- window_layout.win_lay.demo.tcl
+++ window_layout.win_lay.demo.tcl
@@ -32,4 +32,32 @@ proc ::win_lay::MakeSampleWindow { w } {
   toplevel $w ; text $w.t -setgrid 0 ; pack $w.t -padx 5 -pady 5 -fill both
 }
 
-::win_lay::MeasureScreen [list {} {::win_lay::Make4Windows}]
+proc ::win_lay::test1 {} { 
+ puts {===============test1============}
+ puts {you should see error message like "Failed to start measuring screen"}
+ puts {after that you should see "test1 - A succeeded", should not see "test1 - B succeeded"}
+ ::win_lay::MeasureScreen [list {} {puts "test1 - A succeeded"}] 
+ ::win_lay::MeasureScreen [list {} {puts "test1 - B succeeded"}]
+} 
+
+proc ::win_lay::test2 {} { 
+ puts {===============test2============}
+ puts {you should see error message like "timeout and default screenWidthAvailable ~= 600 "}
+ ::win_lay::MeasureScreen [subst -nocommands [list {} {puts "test2 - measuring finished, ScreenWidthAvailable = \$::win_lay::ScreenWidthAvailable"}]]
+ variable ::win_lay::MeasuringEndDueTime [ expr [clock milliseconds] - 100000 ]
+} 
+
+
+proc ::win_lay::test3 {} {
+ puts {===============test3============}
+  puts {you should see 4 windows layed out pretty}
+  ::win_lay::MeasureScreen [list {} {::win_lay::Make4Windows}]
+}
+
+after 1000 {puts "================= Hold on. Starting tests ... =================="}
+
+after 2000 {::win_lay::test1}
+
+after 4000 {::win_lay::test2}
+
+after 7000 {::win_lay::test3}
