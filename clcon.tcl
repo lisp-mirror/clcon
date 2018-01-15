484c53f3c0885bf3a56583cfa101d593b265c639
budden <unknown>
2018-01-15 21:00:26 +0300

    Вернул потерявшуюся инициализацию ::tkcon::PRIV(CurrentReadtableName)

--- clcon.tcl
+++ clcon.tcl
@@ -347,6 +347,7 @@ proc ::tkcon::Init {args} {
         SwankReplReady   0  
         CurrentPackageName "COMMON-LISP-USER"
         CurrentPackageDisplayName "CL-USER"
+        CurrentReadtableName "CURRENT"
     } {
 	if {![info exists PRIV($key)]} { set PRIV($key) $default }
     }
