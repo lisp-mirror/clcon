3b38ed359ea119e65f850f4e54b4858807eff512
budden <unknown>
2018-02-02 21:48:48 +0300

    переехал SBCL

--- clconcmd.tcl
+++ clconcmd.tcl
@@ -111,7 +111,7 @@ Currently defined commands are: "
 
     # Искать в исходниках СБЦЛ
     proc исбцл { str } {
-      ::clconcmd::иф -типы lisp [string cat $::tkcon::YarRoot "/sbcl/t"] $str
+      ::clconcmd::иф -типы lisp [string cat $::tkcon::YarRoot "/sbcl/1.4.2-win"] $str
     }
 
     # Искать в исходниках ЦЦЛ
